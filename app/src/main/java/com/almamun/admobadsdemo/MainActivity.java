58eeb10480495f18cf7ec39fe9f13222d68ea26c
imalmamun <63267042+imalmamun@users.noreply.github.com>
2020-12-11 03:07:06 +0600

    first...app..learned admob integration

---
+++ app/src/main/java/com/almamun/admobadsdemo/MainActivity.java
@@ -0,0 +1,48 @@
+package com.almamun.admobadsdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnShow;
+    private AdView adview1, adview2;
+    InterstitialAd interstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnShow = findViewById(R.id.bt_show);
+        adview1 = findViewById(R.id.ad_view1);
+        adview2 = findViewById(R.id.ad_view2);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adview1.loadAd(adRequest);
+        adview2.loadAd(adRequest);
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        interstitialAd.loadAd(new AdRequest.Builder().build());
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                interstitialAd.show();
+            }
+        });
+
+    }
+}
